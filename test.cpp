fd36b56803350b34c734caaa9b1c6dee798cb351
Sudeep Singh Gill <gillsudeeps@gmail.com>
2021-06-24 18:08:47 +0530

    On branch main Changes to be committed: 	modified:   test.cpp

--- test.cpp
+++ test.cpp
@@ -4,5 +4,9 @@ using namespace std;
 int main() {
   cout << "Hello, Testing Branching";
   int flag = 1;
+<<<<<<< Updated upstream
   bool edit = true;
+=======
+  bool flag2 = false;
+>>>>>>> Stashed changes
 }
