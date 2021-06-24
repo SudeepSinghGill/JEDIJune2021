501a0dfe01d1ab6d43dc951d4145cb78ef852801
SudeepSinghGill <47475056+SudeepSinghGill@users.noreply.github.com>
2021-06-24 18:12:13 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -4,9 +4,5 @@ using namespace std;
 int main() {
   cout << "Hello, Testing Branching";
   int flag = 1;
-<<<<<<< Updated upstream
   bool edit = true;
-=======
-  bool flag2 = false;
->>>>>>> Stashed changes
 }
